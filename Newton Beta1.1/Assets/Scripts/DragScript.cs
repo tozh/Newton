32d5ae62f3b72463a63c0c2303849d7be33a1888
zhaotong0312 <zhaotong0312@outlook.com>
2017-06-11 02:29:29 -0700

    Beta 1.1
    complete UI

---
+++ Newton Beta1.1/Assets/Scripts/DragScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragScript : MonoBehaviour {
+
+	public Transform tform;
+	// Use this for initialization
+	void Start () {
+		tform = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	public void UpdateDrag (Vector2 mousePos) {
+		transform.position = mousePos;
+	}
+}
