b6d6a7866a87f55a8b83650a687d0a62da9f2076
Chufan Deng <jerry.dengcf@gmail.com>
2017-07-10 09:20:35 -0700

    final

---
+++ 526 final/Assets/Scripts/Istutor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Istutor : MonoBehaviour {
+
+    private List<tutorial> my_tutor;
+    public LineCreatorScript line;
+    // Use this for initialization
+
+    void Start () {
+
+        gameObject.SetActive(false);
+        string name = SceneManager.GetActiveScene().name;
+        string id = name.Substring(5);
+
+        my_tutor = tutor_firstplay.LoadTutor();
+        foreach(tutorial l in my_tutor)
+        {
+            if(l.levelID == id)
+            {
+                if (l.tutor == true)
+                {
+                    gameObject.SetActive(true);
+                    line.setCanDraw(false);
+                    //Debug.Log(line.canDraw);
+                    tutor_firstplay.SetTutor(id);
+                }
+                else
+                {
+                    gameObject.SetActive(true);
+                    line.setCanDraw(false);
+                    //Debug.Log(line.canDraw);
+                }
+            }
+        }
+
+    }
+}
